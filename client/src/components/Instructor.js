0b54f0810e0460daf9749dcc0689082358a103eb
ebisLab <ebaiden@techie.com>
2020-03-02 23:53:39 -0500

    mock data

--- client/src/components/Instructor.js
+++ client/src/components/Instructor.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Card from './Card'
 import AddClass from './AddClass'
 
+
+const infoData =[{
+    id:0,
+  event: 'Yoga Retreat',
+  rating: 0,
+  complete: false,
+  event_code: '7890',
+  },
+  {
+    id:1,
+  event: 'p90x',
+  rating: 0,
+  complete: false,
+  event_code: '7890',
+  }]
+  
 const Instructor = () =>{
+    const [data, setData] = useState([])
+    // const [data, setData] = useState([])
+
+
+      useEffect(() => {
+    //once get data inputs are created
+    setData(infoData)
+      }, [])
+
+console.log(data, 'data')
 return(
     <div>
             <h2>Hello this is the instuctor</h2>
---
+++ client/src/components/Sample.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Sample = ()=>{
+    return(<div>
+        <h2>Sample Section</h2>
+    </div>)
+}
+
+export default Sample

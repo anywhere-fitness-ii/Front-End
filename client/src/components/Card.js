08b86fe6f3e9dbf7f2a0123e06f1eb32e3dc8704
ebisLab <ebaiden@techie.com>
2020-03-02 23:34:50 -0500

    card component added, instructor set up

---
+++ client/src/components/Card.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Card = ()=>{
+    return(<div>
+        <h2>Card Section</h2>
+    </div>)
+}
+
+export default Card
--- client/src/components/Instructor.js
+++ client/src/components/Instructor.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import Card from './Card'
+import AddClass from './AddClass'
 
 const Instructor = () =>{
-return(<div>
-    <h2>Hello this is the instuctor</h2>
-</div>)
+return(
+    <div>
+            <h2>Hello this is the instuctor</h2>
+
+<div style={{display: 'inline-flex'}}>
+    <AddClass />
+    <Card />
+    
+</div>
+</div>
+)
 }
 
 export default Instructor;

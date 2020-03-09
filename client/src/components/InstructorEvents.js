17f38b7aafe43d8c71d66d4efd4e0dc960becf84
ebisLab <ebaiden@techie.com>
2020-03-08 22:14:42 -0400

    date format adjusted

--- client/src/components/InstructorEvents.js
+++ client/src/components/InstructorEvents.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const InstructorEvents = (props)=>{
-console.log(props, 'instructor event')
+
+    let moment = require('moment');
+    moment().format();
+
     return(
     <div style={{border: '1px solid #C4C4C4', padding: "40px", borderRadius: "10px", margin: "10px", marginBottom: '40px'}}>
         <h2>Upcoming events</h2>       
@@ -10,13 +13,12 @@ console.log(props, 'instructor event')
                     return item.class_max_participants == 1;
                 }).map(item=> (
                     <div key={item.id} style={{border: '1px solid #C4C4C4', padding: "40px", borderRadius: "10px", margin: "20px"}}>
-                        <div style={{display: 'inline-flex' }}>
+                        <div style={{display: 'inline-flex', margin:'20px 0px' }}>
                             <span style={{marginRight: '10px'}}>📅</span>
-                        <h2>{item.class_date}</h2>
+                            <sub><h1>{moment(item.class_date).format('DD')}</h1></sub>
+                            <sup><h4 style={{fontWeight: '100'}}>{moment(item.class_date).format('MMM').toUpperCase()}</h4></sup>
                         </div>
-                        <p>{item.class_type}</p>
-                        {console.log(item.class_img_url)}
-                        
+                        <p>{item.class_type}</p>                      
                         </div>))}
                         
                         </div>

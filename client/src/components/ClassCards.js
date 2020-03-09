a4f95d9058e9bf9bb43bcd308338f3af55ce4083
ebisLab <ebaiden@techie.com>
2020-03-09 10:19:33 -0400

    removed unnecessary console.log

--- client/src/components/ClassCards.js
+++ client/src/components/ClassCards.js
@@ -24,17 +24,17 @@ const ClassCards = ({classInstance, setCardToUpdate, setEditing, toggleItem})=> 
   return (
       <Card className="mt-3">
         <CardBody className="cardStyle">
-          
+          {console.log(classInstance.class_date, 'class date from instance')}
           <CardSubtitle style={{display: 'inline-flex', margin:'20px 0px'}}>
             <sub><h1>{moment(classInstance.class_date).format('DD')}</h1></sub>
             <sup><h5 style={{fontWeight: '100'}}>{moment(classInstance.class_date).format('MMM').toUpperCase()}</h5></sup>
           </CardSubtitle>
          <CardSubtitle><strong>Class Name: </strong>{classInstance.class_name}</CardSubtitle>
-          <CardSubtitle className="mt-1"><strong>Class Type: </strong>{classInstance.class_type}</CardSubtitle>
+          {/* <CardSubtitle className="mt-1"><strong>Class Type: </strong>{classInstance.class_type}</CardSubtitle> */}
           {/* <CardSubtitle className="mt-1"><strong>Date: </strong>{moment(classInstance.class_date).format('MM/DD/YYYY')}</CardSubtitle> */}
           <CardSubtitle className="mt-1"><strong>Start Time: </strong>{classInstance.class_start_time}</CardSubtitle>
           <CardSubtitle className="mt-1"><strong>Duration: </strong>{classInstance.class_duration}</CardSubtitle>
-          <CardSubtitle className="mt-1"><strong>Intensity: </strong>{classInstance.class_intensity}</CardSubtitle>
+          {/* <CardSubtitle className="mt-1"><strong>Intensity: </strong>{classInstance.class_intensity}</CardSubtitle> */}
           <CardSubtitle className="mt-1"><strong>Location: </strong>{classInstance.class_location}</CardSubtitle>
           {/* <CardSubtitle className="mt-1"><strong>Registered Participants: </strong>{classInstance.registered_participants}</CardSubtitle> */}
           <CardSubtitle className="mt-1"><strong>Max Participants: </strong>{classInstance.class_max_participants}</CardSubtitle>
